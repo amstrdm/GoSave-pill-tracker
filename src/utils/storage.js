ef3c47baf301a1eeea14db9b1c427cbee8430e9f
amstrdm <a.caserta@proton.me>
2024-09-23 18:07:38 +0200

    added functions to save/get Cycle Data to/from local Storage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,5 +1,5 @@
 export const saveIntakeSettings = (settings) => {
-    console.log("settings: " + settings)
+    console.log("Intake settings: " + settings)
     localStorage.setItem("intakeTime", JSON.stringify(settings))
     //localStorage.setItem("cycleDates", cycleDates)
 }
@@ -8,4 +8,21 @@ export const getIntakeSettings = () => {
     const settings = localStorage.getItem("intakeTime")
     //const cycleDates = localStorage.getItem("cycleDates")
     return settings ? JSON.parse(settings) : null
+}
+
+export const saveCycleSettings = (cycleData) => {
+    const cycleDataJSON = JSON.stringify(cycleData)
+
+    localStorage.setItem("cycleSettings", cycleDataJSON)
+    console.log("Cycle settings saved to local storage:", cycleData);
+}
+
+export const getCycleSettings = () => {
+    const savedSettings = localStorage.getItem("cycleSettings")
+
+    if (savedSettings) {
+        return JSON.parse(savedSettings)
+    }else{
+        return null
+    }
 }
