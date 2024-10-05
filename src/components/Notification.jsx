57b5b0fff75b625400c2af850c73565c16a2c944
amstrdm <a.caserta@proton.me>
2024-10-05 15:20:27 +0200

    Added react Notification Component for requesting notification permission, sending FCM token to backend and handling notifications while App is in focus

---
+++ src/components/Notification.jsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import { messaging } from "../firebase";
+import { getToken, onMessage } from "firebase/messaging";
+import axiosInstance from "../axiosInstance";
+ 
+
+function Notification() {
+  useEffect(() => {
+    const requestPermission = async () => {
+      try {
+        // Wait for the service worker registration
+        const registration = await navigator.serviceWorker.ready;
+        
+        // Request the token
+        const token = await getToken(messaging, {
+          vapidKey: 'BK0qwUVTs45pLrpte0VlRRYkQyQfzAKwIINcrj_R1B-dtYIWUZKV5aS4F5ii-XhXT5ZSZG9YqLIjBRm54CcVkwI',
+          serviceWorkerRegistration: registration, // Pass the service worker registration
+        });
+
+        if (token) {
+          console.log("Token generated: ", token);
+          
+          // Send token to the backend
+          await axiosInstance.post("/save-token", { token }, {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          console.log("Token saved successfully:", token);
+        } else {
+          console.log("No registration token available.");
+        }
+      } catch (err) {
+        console.error("Error getting token: ", err);
+      }
+    };
+
+    // Handle incoming messages while the app is in the foreground
+    const unsubscribe = onMessage(messaging, (payload) => {
+      console.log("Message received. ", payload);
+    });
+
+    requestPermission();
+
+    // Clean up subscription on unmount
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return null;
+}
+
+export default Notification;
