90e43410d97b31ca63c4a95b884d1f1457d39dc9
amstrdm <a.caserta@proton.me>
2024-10-05 16:03:38 +0200

    added firebase messaging service worker to handle background notifications

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebase.initializeApp({
+  apiKey: "***REMOVED***",
+  authDomain: "***REMOVED***",
+  projectId: "***REMOVED***",
+  storageBucket: "***REMOVED***.appspot.com",
+  messagingSenderId: "***REMOVED***",
+  appId: "1:***REMOVED***:web:075a4f09ee1987599f874d",
+  measurementId: "***REMOVED***"
+});
+
+const messaging = firebase.messaging()
+
+messaging.onBackgroundMessage((payload) => {
+  console.log("Received background message ", payload)
+})
