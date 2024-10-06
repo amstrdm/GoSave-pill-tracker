8ccd0aea7d5c3140c69edc2c42ce9bee538f3bb8
amstrdm <a.caserta@proton.me>
2024-10-06 02:11:41 +0200

    Changed axiosinstance variable name to 'api' from 'axiosInstance'

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,11 +1,11 @@
 // src/axiosInstance.js
 import axios from 'axios';
 
-const axiosInstance = axios.create({
+const api = axios.create({
   baseURL: 'http://localhost:5000', 
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-export default axiosInstance;
+export default api;
