6926b5ba2cd735864ead22c50964b0c6800ac3e1
amstrdm <a.caserta@proton.me>
2024-10-11 14:47:50 +0200

    added icon for background notifications

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -21,7 +21,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: './contraceptive-pill.png', 
+    icon: 'contraceptive-pill.png', 
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
