ec9a53ef7a56743bdc14a0593460a6a2ca92c79a
amstrdm <a.caserta@proton.me>
2024-10-15 12:19:08 +0200

    exported changeIntakeTime and RemindHome into their own components

--- Frontend/src/components/ChangeIntakeTime.jsx
+++ Frontend/src/components/ChangeIntakeTime.jsx
@@ -1,8 +1,17 @@
 
 
 function ChangeIntakeTime() {
+    
+    function handleChangeIntakeTime(){
+
+    }
+
     return (
-        <button>Change Intake Time</button>
+        <button className="btn mt-12 ml-5 mr-5" onClick={handleChangeIntakeTime}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>Change Intake Time
+        </button>
     )
 }
 
